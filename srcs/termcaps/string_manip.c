eba5ff39a8b239b2fa2e93cb26efa476a78c0920
Solal Dunckel <sdunckel@student.42.fr>
2020-03-03 16:22:20 +0100

    cut/copy/paste fix

--- srcs/termcaps/string_manip.c
+++ srcs/termcaps/string_manip.c
@@ -6,7 +6,7 @@
 /*   By: haguerni <haguerni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/26 21:01:10 by haguerni          #+#    #+#             */
-/*   Updated: 2020/02/27 20:50:18 by sdunckel         ###   ########.fr       */
+/*   Updated: 2020/03/03 16:00:00 by sdunckel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,28 +39,72 @@ char	*ft_strjoin_middle(char *s1, const char *s2, int div)
 	return (str);
 }
 
+char	*ft_strjoin_middle2(char *s1, const char *s2, int div)
+{
+	char	*str;
+	int		l;
+	char	*tmp;
+
+	if (!s2)
+	{
+		str = ft_strdup(s1);
+		free((void*)s1);
+		return (str);
+	}
+	l = ft_strlen(s1) + ft_strlen(s2);
+	if (!(str = (char*)malloc(sizeof(char) * (l + 1))))
+		return (NULL);
+	ft_strlcpy(str, s1, div + 1);
+	ft_strcat(str, s2);
+	tmp = ft_substr(s1, div, ft_strlen(s1) - div);
+	ft_strcat(str, tmp);
+	free(tmp);
+	return (str);
+}
+
 void	cut_line(void)
 {
+	char	*to_free;
+
+	to_free = NULL;
 	if (!g_minishell->line)
 		return ;
-	g_tc->copy_cmd = ft_strdup(g_minishell->line);
-	g_tc->cur_pos = 0;
-	g_minishell->line = NULL;
+	to_free = g_tc->copy_cmd;
+	g_tc->copy_cmd = ft_substr(g_minishell->line, g_tc->cur_pos,
+		ft_strlen(g_minishell->line) - g_tc->cur_pos);
+	free(to_free);
+	to_free = g_minishell->line;
+	g_minishell->line = ft_substr(g_minishell->line, 0, g_tc->cur_pos);
+	free(to_free);
 }
 
 void	copy_line(void)
 {
+	char	*to_free;
+
+	to_free = NULL;
 	if (!g_minishell->line)
 		return ;
-	g_tc->copy_cmd = ft_strdup(g_minishell->line);
+	to_free = g_tc->copy_cmd;
+	g_tc->copy_cmd = ft_substr(g_minishell->line, g_tc->cur_pos,
+		ft_strlen(g_minishell->line) - g_tc->cur_pos);
+	free(to_free);
 }
 
 void	paste_line(void)
 {
+	char	*to_free;
+
+	to_free = NULL;
 	if (!g_tc->copy_cmd)
 		return ;
 	if (g_minishell->line)
-		g_minishell->line = ft_strjoin_free(g_minishell->line, g_tc->copy_cmd);
+	{
+		to_free = g_minishell->line;
+		g_minishell->line = ft_strjoin_middle2(g_minishell->line,
+			g_tc->copy_cmd, g_tc->cur_pos);
+		free(to_free);
+	}
 	else
 		g_minishell->line = ft_strdup(g_tc->copy_cmd);
 	g_tc->cur_pos += ft_strlen(g_tc->copy_cmd);
